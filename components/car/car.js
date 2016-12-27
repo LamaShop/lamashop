23b04d82f9d384cbfa9bc66775a9006573aa17d7
yanyan <1107380269@qq.com>
2016-12-27 18:32:43 +0800

    'zhao12.27第二次'

--- components/car/car.js
+++ components/car/car.js
@@ -1,5 +1,7 @@
 angular.module('CarModule',[])
-
     .controller('carCtrl',['$scope',function($scope){
-
+        $scope.backImg=true;
+        $scope.changeBack=function () {
+            $scope.backImg=!$scope.backImg;
+        }
     }])
