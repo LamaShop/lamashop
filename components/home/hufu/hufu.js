8c66a7e3b5cca0a4270d7c906b8deb2597d06d08
luobin <930457504@qq.com>
2016-12-28 18:12:01 +0800

    '12/28'

--- components/home/hufu/hufu.js
+++ components/home/hufu/hufu.js
@@ -7,7 +7,7 @@ angular.module('HufuModule',[])
         return $http.get('json/skin.json');
     }])
     .service('dataService31',['$http',function($http){
-        return $http.get('json/jsonchoose2.json');
+        return $http.get('json/jsonchoose4.json');
     }])
     .controller('hufuCtrl',['$scope','dataService30','dataService31',function($scope,dataService30,dataService31){
         dataService30.success(function(res){
@@ -21,4 +21,52 @@ angular.module('HufuModule',[])
         $scope.changeOrderType = function(type){
             $scope.orderType = type;
         }
+
+        $scope.arrShow = [];
+        $scope.text =false;
+        dataService31.success(function(res){
+            $scope.arr2 = res.data.attr;
+            $scope.arr3 = res.data.attr[0].list;
+            $scope.getText = function(i,obj){
+                console.log(i);
+                console.log(obj);
+                $scope.isShow = true;
+                $scope.arr3 = [];
+                $scope.arr3 = res.data.attr[i].list;
+                $scope.text =true;
+
+            }
+
+
+            //点击筛选,触发事件
+            $scope.presss = function(){
+                $scope.isShow = true;
+            }
+            //点击遮罩层,遮罩层消失;
+            $scope.getHide = function(){
+                $scope.isShow = false;
+            }
+
+            $scope.arr4 ="";
+            $scope.pressImg = function(j){
+
+                $scope.num = j;
+                $scope.arr4 = [];
+                $scope.arr4 = res.data.attr[0].list[j].text;
+
+            }
+            //重置按钮
+            $scope.oneMore = function(){
+                $scope.isShow = false;
+                $scope.pressWord=false;
+                $scope.arr4 = "";
+            }
+            $scope.aShow = true;
+            $scope.bShow = false;
+            $scope.show = function(){
+                $scope.aShow = !$scope.aShow;
+                $scope.bShow = !$scope.bShow;
+            }
+        })
+
     }])
